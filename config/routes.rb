fbe6eba47eb2038d0666b1f74ee7adc7dcce97c0
Bookis Smuin <vegan.bookis@gmail.com>
2012-10-08 15:43:24 -0700

    add migrate route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Configurator::Application.routes.draw do
-  resources :remote_apps, path: "/remote-app/"
-    root :to => 'remote_apps#index'
+  resources :remote_apps do
+    get :migrate, on: :member
+  end
+  root :to => 'remote_apps#index'
 end
