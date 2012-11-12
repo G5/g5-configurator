9c74bc76759b3c541e16978a7695ca3e74bf5306
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-11-12 11:29:38 -0800

    updates resque initializer

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,12 @@
-ENV["REDISTOGO_URL"] ||= "redis://localhost:6379/"
+# Setup for Heroku
+Resque.redis = ENV["REDISTOGO_URL"] if ENV["REDISTOGO_URL"]
 
-uri = URI.parse(ENV["REDISTOGO_URL"])
-Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
+# Establish connection to the database before each job
+Resque.before_fork = Proc.new { ActiveRecord::Base.establish_connection }
+
+# Require HTTP Basic Auth to view dashboard if set
+if ENV["RESQUE_HTTP_USER"] && ENV["RESQUE_HTTP_PASSWORD"]
+  Resque::Server.use(Rack::Auth::Basic) do |user, password|
+    user == ENV["RESQUE_HTTP_USER"] and password == ENV["RESQUE_HTTP_PASSWORD"]
+  end
+end
