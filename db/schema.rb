a1e77f6650f12657a116284b7e47a1ff0e3af20e
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-11-15 17:56:28 -0800

    adds git repo to instruction view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121115230745) do
+ActiveRecord::Schema.define(:version => 20121115233605) do
 
   create_table "entries", :force => true do |t|
     t.string   "bookmark"
