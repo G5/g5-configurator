d5b3d940468e6937e8d6875d18d04de0da8e2bb6
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-12-10 15:47:34 -0800

    removes heroku url from remote apps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121209205956) do
+ActiveRecord::Schema.define(:version => 20121210234615) do
 
   create_table "entries", :force => true do |t|
     t.string   "uid"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(:version => 20121209205956) do
   create_table "remote_apps", :force => true do |t|
     t.integer  "entry_id"
     t.string   "name"
-    t.string   "heroku_url"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "kind"
