54f1d0e895da4b20f34a951f6abfa4f7bc348aeb
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-12-17 10:02:13 -0800

    creates client app creator

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 RemoteApp.skip_callback(:create, :after, :create_instruction)
-RemoteApp.client_app_creator
+RemoteApp.create(kind: CLIENT_APP_CREATOR)
 RemoteApp.set_callback(:create, :after, :create_instruction)
