b6ccec46e67084b51b071af34a805ee60259d36f
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-01-07 10:08:46 -0800

    adds specs for RemoteApp

--- spec/models/remote_app_spec.rb
+++ spec/models/remote_app_spec.rb
@@ -34,4 +34,32 @@ describe RemoteApp do
   its(:git_repo) { should be_present }
   its(:heroku_repo) { should be_present }
   its(:heroku_url) { should be_present }
+
+  describe "#heroku_repo" do
+    it "should use heroku_app_name to create heroku repo" do
+      @app.stub(:heroku_app_name).and_return("mock-app")
+      @app.heroku_repo.should == "git@heroku.com:mock-app.git"
+    end
+  end
+  describe "#heroku_url" do
+    it "should use heroku_app_name to create heroku url" do
+      @app.stub(:heroku_app_name).and_return("mock-app")
+      @app.heroku_url.should == "http://mock-app.herokuapp.com"
+    end
+  end
+  describe "#siblings" do
+    before :each do
+      @sibling = RemoteApp.create!(
+        kind: RemoteApp::CLIENT_HUB_DEPLOYER,
+        client_name: "mock client",
+        client_uid: @app.client_uid
+      )
+    end
+    it "returns apps with same client uid" do
+      @app.siblings.should include @sibling
+    end
+    it "does not return self" do
+      @app.siblings.should_not include @app
+    end
+  end
 end
