70f3c8c0fc40e3e172623cfa6b3b74d4b23dcadc
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-01-08 21:48:11 -0500

    cleans up spec helper and makes simplecov run with spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,33 +1,40 @@
-require 'simplecov'
-SimpleCov.start 'rails'
-
 require 'rubygems'
 require 'spork'
 
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
 Spork.prefork do
+  unless ENV['DRB']
+    require 'simplecov'
+    SimpleCov.start 'rails'
+  end
+
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+
+  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
   RSpec.configure do |config|
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
     config.use_transactional_fixtures = true
     config.infer_base_class_for_anonymous_controllers = false
     config.order = "random"
-    config.before(:each) do 
-      RemoteApp.delete_all
-      Instruction.delete_all
+
+    config.before :each do
+      # create client app creator
       RemoteApp.skip_callback(:create, :after, :create_instruction)
-      RemoteApp.create(name: "g5-client-app-creator", git_repo: "git@git")
+      RemoteApp.create!(kind: RemoteApp::CLIENT_APP_CREATOR)
       RemoteApp.set_callback(:create, :after, :create_instruction)
     end
   end
+
   Spork.trap_method(Rails::Application, :eager_load!)
   require File.expand_path("../../config/environment", __FILE__)
   Rails.application.railties.all { |r| r.eager_load! }
 end
 
 Spork.each_run do
+  if ENV['DRB']
+    require 'simplecov'
+    SimpleCov.start 'rails'
+  end
 end
