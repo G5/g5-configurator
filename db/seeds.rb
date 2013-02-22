f9c2c908370d700011ecc232b4683a8f561b5241
Michael Mitchell <michael.mitchell@g5searchmarketing.com>
2013-02-22 10:36:14 -0800

    fix typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 RemoteApp.skip_callback(:create, :after, :create_instruction)
 RemoteApp.create(kind: RemoteApp::CLIENT_APP_CREATOR)
-RemoteApp.create(:kind RemoteApp::CLIENT_APP_CREATOR_DEPLOYER)
+RemoteApp.create(kind: RemoteApp::CLIENT_APP_CREATOR_DEPLOYER)
 RemoteApp.set_callback(:create, :after, :create_instruction)
