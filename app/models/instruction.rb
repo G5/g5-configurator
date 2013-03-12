884e85de6a5855ab59e49eff0ce22f67af4e85c4
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-03-11 19:28:54 -0700

    adds default values for env vars in config/initializers/env.rb and moves webhook to remote app model

--- app/models/instruction.rb
+++ app/models/instruction.rb
@@ -56,14 +56,11 @@ class Instruction < ActiveRecord::Base
     Resque.enqueue(InstructionWebhooker, self.id)
   end
 
+  def webhook_url
+  end
+
   def webhook_target_apps
-    target_apps.map(&:heroku_app_name).each do |heroku_app_name|
-      begin
-        Webhook.post("http://#{heroku_app_name}.herokuapp.com/webhooks/g5-configurator")
-      rescue ArgumentError => e
-        logger.error e
-      end
-    end
+    target_apps.map(&:webhook)
   end
 
   def client_app_creator_kind?
