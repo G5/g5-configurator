d004378158c4b2adb2deafaf413c02a1d0a08c6c
jessicard <jessicard@mac.com>
2014-03-06 17:47:23 -0800

    remove all instructions in migration

--- db/migrate/20140304220845_update_client_hub_deployer_to_client_app_updater.rb
+++ db/migrate/20140304220845_update_client_hub_deployer_to_client_app_updater.rb
@@ -1,6 +1,7 @@
 class UpdateClientHubDeployerToClientAppUpdater < ActiveRecord::Migration
   def up
     RemoteApp.where(kind: "client-hub-deployer").each do |app|
+      Instruction.destroy_all
       app.kind = "client-app-updater"
       app.name = nil
       app.name = app.send(:assign_name)
