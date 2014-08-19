2bb8ddac01995262973d239a221b1d4b59b6f50d
Michael Mitchell <michael.mitchell@getg5.com>
2014-08-19 12:28:14 -0700

    adds organization to RemoteApp and instructions views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140812174627) do
+ActiveRecord::Schema.define(version: 20140819191024) do
 
   create_table "entries", force: true do |t|
     t.string   "uid"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20140812174627) do
     t.string   "client_uid"
     t.string   "client_name"
     t.string   "heroku_app_name"
+    t.string   "organization"
   end
 
 end
