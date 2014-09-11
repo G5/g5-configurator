7eaa5155513595b723f1928d9e71929d1ae3b331
Chris Tsongas <chris.tsongas@getg5.com>
2014-09-11 15:36:26 -0700

    update inv vars and test coverage

--- config/initializers/env.rb
+++ config/initializers/env.rb
@@ -1,11 +1,11 @@
 ENV["G5_HUB_ENTRIES_URL"] ||= case Rails.env
-  when "production"  then "https://g5-hub.g5dxm.com"
-  when "development" then "http://g5-hub.dev"
-  when "test"        then "http://g5-hub.test"
+  when "production"  then "https://hub.g5dxm.com"
+  when "development" then "http://hub.dev"
+  when "test"        then "http://hub.test"
 end
 
 ENV["G5_REMOTE_APP_WEBHOOK_HOST"] ||= case Rails.env
-  when "production"  then "g5dxm.com"
+  when "production"  then "herokuapp.com"
   when "development" then "dev"
   when "test"        then "test"
 end
--- spec/lib/g5_hub_service_spec.rb
+++ spec/lib/g5_hub_service_spec.rb
@@ -4,5 +4,5 @@ describe G5HubService do
   before do
     G5AuthenticationClient::Client.stub_chain(:new, :get_access_token).and_return("the_token")
   end
-  its(:feed_url) { should eq("http://g5-hub.test?access_token=the_token") }
+  its(:feed_url) { should eq("http://hub.test?access_token=the_token") }
 end
