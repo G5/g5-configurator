06cde049e56395e6b6b40122ba691cad74396911
Michael Mitchell <michael.mitchell@getg5.com>
2014-10-16 13:05:56 -0700

    adds logging in remote app client app creator create instruction

--- app/models/remote_app.rb
+++ app/models/remote_app.rb
@@ -87,6 +87,8 @@ class RemoteApp < ActiveRecord::Base
   end
 
   def create_instruction
+    Rails.logger.info("Creating instruction in remote_app")
+    Rails.logger.info("client app creator: #{client_app_creator")
     Instruction.create(
       target_app_kind: client_app_creator.kind,
       target_app_ids: [client_app_creator.id],
