fa524e9ac8415dcf3dbcff883ad0408303b98f38
bbauer <brian.bauer@g5platform.com>
2014-11-07 10:57:46 -0800

    Send access token with update url

--- app/workers/garden_updater.rb
+++ app/workers/garden_updater.rb
@@ -5,10 +5,14 @@ class GardenUpdater
   def self.perform(update_type, app_names)
     app_names.each do |app_name|
       begin
-        HTTParty.put("https://#{app_name}.herokuapp.com" \
-                     "/garden_updates/#{update_type}")
+        HTTParty.put("https://#{app_name}.herokuapp.com/garden_updates/" \
+                     "#{update_type}?access_token=#{access_token}".strip)
       rescue
       end
     end
+  end
+
+  def self.access_token
+    G5AuthenticationClient::Client.new.get_access_token
   end
 end
--- spec/workers/garden_updater_spec.rb
+++ spec/workers/garden_updater_spec.rb
@@ -1,7 +1,11 @@
 require "spec_helper"
 
 describe GardenUpdater do
-  before { HTTParty.stub(:put) }
+  before do
+    HTTParty.stub(:put)
+    G5AuthenticationClient::Client.stub_chain(:new, :get_access_token).
+                                   and_return("the_token")
+  end
 
   describe ".perform" do
     let(:app_names) { ["foo"] }
@@ -9,7 +13,8 @@ describe GardenUpdater do
 
     it "PUTs to the updater path in each selected app" do
       expect(HTTParty).to receive(:put).
-        with("https://foo.herokuapp.com/garden_updates/garden_web_layout")
+        with("https://foo.herokuapp.com/garden_updates/" \
+             "garden_web_layout?access_token=the_token")
 
       GardenUpdater.perform(update_type, app_names)
     end
